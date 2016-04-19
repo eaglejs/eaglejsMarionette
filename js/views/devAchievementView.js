3e4ff281430ba71160b8e5109848d4c2f43bc2e1
jeagle <joshuaseagle@gmail.com>
2016-04-19 18:17:00 -0400

    Added col-sm-3 for larger screens.

--- js/views/devAchievementView.js
+++ js/views/devAchievementView.js
@@ -4,7 +4,7 @@ define(function (require) {
     var template = require('text!../../html/devAchievementItem.html');
     
     return Backbone.Marionette.ItemView.extend({
-        className: 'col-xs-4',
+        className: 'col-xs-4 col-sm-3',
         template: _.template(template)
     });
 });
